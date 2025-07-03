a0b4aee9d5a295c211d1c9bbe5c38a2ffa49f846
SoorajVijayan <soorajvijayan@revyrieglobal.com>
2025-07-03 23:03:39 +0530

    Add API route for update and delete todo

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -40,12 +40,12 @@ router.get('/',VerifyToken,async (req, res) =>
 
 router.post('/', async (req, res) =>
 {
-  const {title,userId}=req.body
+  const { title, userId } = req.body
   const createTodo = await prisma.todo.create({
     data: {
       title: title,
       status: 'TODO',
-      userId:parseInt(userId)
+      userId: parseInt(userId)
     }
    
   })
@@ -58,6 +58,53 @@ router.post('/', async (req, res) =>
   {
     res.status(400).json({ status: false, message: 'Unable to create todo' });
   }
-})
+});
 
+router.delete('/:id', async (req, res) =>
+{
+  const Id = parseInt(req.params.id);
+  try
+  {
+    const deleteTodo = await prisma.todo.delete({
+      where: {
+        id: Id,
+      },
+    });
+    if (deleteTodo)
+    {
+      res.status(200).json({ message: 'Todo deleted successfully' });
+    }
+  } catch (error)
+  {
+    res.status(400).json({ message: 'Failed to delete todo' })
+  }
+
+
+});
+
+router.patch('/:id', async (req, res) =>
+{
+  const Id = parseInt(req.params.id);
+  const updatedData = req.body.title;
+
+  try {
+      const updateTodo = await prisma.todo.update({
+    where: {
+      id: Id
+    },
+    data: {
+      title: updatedData
+    }
+      });
+    if (updateTodo)
+    { 
+      res.status(200).json({status:true, message: 'Todo updated successfully' });
+    }
+  } catch (error) {
+     res.status(400).json({ status:false,message: 'failed to update todo' });
+  }
+
+}
+
+)
 module.exports = router;
